b0d77e9e23836c220a8ee5ade79de3478fef928d
PriyanshuVaity27 <vaitypriyanshu@gmail.com>
2024-08-07 14:45:10 +0530

    Hello.py

---
+++ Hello.py
@@ -0,0 +1,6 @@
+# This is a basic Python script demonstrating several commands and concepts.
+
+# Function to print a welcome message
+def welcome_message():
+    print("Welcome to the Python program!")
+  
